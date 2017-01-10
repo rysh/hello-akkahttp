b3925b99ac9b5a99fe2229960eac6f9e7202e018
rysh <rysh.cact@gmail.com>
2017-01-10 19:45:47 +0900

    marshalling and unmarshalling JSON

--- src/main/scala/com/example/WebServer.scala
+++ src/main/scala/com/example/WebServer.scala
@@ -1,27 +1,65 @@
 package com.example
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.Done
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
 import scala.io.StdIn
+
+import scala.concurrent.Future
 /**
   * Created by rysh on 2017/01/10.
   */
 object WebServer {
+
+  // domain model
+  final case class Item(name: String, id: Long)
+  final case class Order(items: List[Item])
+
+  // formats for unmarshalling and marshalling
+  implicit val itemFormat = jsonFormat2(Item)
+  implicit val orderFormat = jsonFormat1(Order)
+
+  // (fake) async database query api
+  def fetchItem(itemId: Long): Future[Option[Item]] = ???
+  def saveOrder(order: Order): Future[Done] = ???
+
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
+    // needed to run the route
+    implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
+    // needed for the future map/flatmap in the end
     implicit val executionContext = system.dispatcher
 
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+    val route: Route =
+      get {
+        pathPrefix("item" / LongNumber) { id =>
+          // there might be no item for a given id
+          println(id)
+          val maybeItem: Future[Option[Item]] = fetchItem(id)
+
+          onSuccess(maybeItem) {
+            case Some(item) => complete(item)
+            case None       => complete(StatusCodes.NotFound)
+          }
         }
-      }
+      } ~
+        post {
+          path("create-order") {
+            entity(as[Order]) { order =>
+              val saved: Future[Done] = saveOrder(order)
+              onComplete(saved) { done =>
+                complete("order created")
+              }
+            }
+          }
+        }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
